57b2a3aef4dfeecc7ea3e4bb8162d581142118e8
Stan Ulric <114952772+YifferDev@users.noreply.github.com>
2022-10-17 17:57:17 -0300

    #SapoGuinuche
    Rebbit

---
+++ SapoGuinuche/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace SapoGuinuche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+  
+           int n = int.Parse(Console.ReadLine());
+
+           for (int i = 1; i <= n; i++)
+           {
+               int primeiro = i;
+               int segundo = i * i;
+               int terceiro = i * i * i;
+               Console.WriteLine($"{primeiro} {segundo} {terceiro}");
+           }
+             
+
+======================================================================================================================================================================================================
+
+
+          int n = int.Parse(Console.ReadLine());
+
+          for (int i = 1; i <= n; i++)
+          {
+              if (n % i == 0)
+              {
+                  Console.WriteLine(i + " é mesmo é");
+              }
+          }
+
+
+======================================================================================================================================================================================================
+
+
+              int n = int.Parse(Console.ReadLine());
+
+              for (int i = 0; i < n; i++)
+              {
+                  string[] line = Console.ReadLine().Split(' ');
+                  double a = double.Parse(line[0]);
+                  double b = double.Parse(line[1]);
+
+                  if (b == 0)
+                  {
+                      Console.WriteLine("divisao impossivel else é mesmo é");  
+                  }
+
+                  else
+                  {
+                      double div = (double)a / b;
+                      Console.WriteLine(div.ToString("F1"));
+                      Console.WriteLine("é mesmo é");
+                  }
+              }
+
+
+
+======================================================================================================================================================================================================
+
+
+
+              Console.WriteLine("qauntos valores vc deseja digitar?");
+              int n = int.Parse(Console.ReadLine());
+
+              for (int i = 0; i < n; i++)
+              {
+                  Console.WriteLine("digite um conjunto de valores:");
+                  string[] line = Console.ReadLine().Split(' ');
+                  double a = double.Parse(line[0]);
+                  double b = double.Parse(line[1]);
+                  double c = double.Parse(line[2]);
+
+                  double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+
+                  Console.WriteLine(media.ToString("F1"));
+                  Console.WriteLine("é mesmo é");
+                  */
+        }
+    }
+}
